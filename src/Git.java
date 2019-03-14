8429c7356c3eac63543748134e29434743625c5b
gowthamnagaraj94 <gowthamnagaraj1234@gmail.com>
2019-03-14 13:56:19 +0530

    message

--- src/Git.java
+++ src/Git.java
@@ -3,11 +3,12 @@ public class Git {
 
 	public static void main(String[] args) {
 		
-	int num[]=new int[5];
+	int num[]=new int[6];
 	num[0]=1;
 	num[1]=2;
 	num[2]=3;
 	num[4]=5;
+	num[5]=6;
 	System.out.println(num.length);
 	System.out.println(num[1]);
 
