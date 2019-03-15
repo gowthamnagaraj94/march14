9e6a95892381d7849d08f6a44f88e99cbec3f982
DevikaTesting <k.devi.divi@gmail.com>
2019-03-15 17:18:58 +0530

    list added

---
+++ src/march14/Sample.java
@@ -0,0 +1,15 @@
+package march14;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sample {
+	public static void main(String[] args) {
+		List <Integer> li=new ArrayList();
+		li.add(10);
+		li.add(20);
+		li.add(30);
+		System.out.println(li);
+	}
+
+}
